f0cc7b3eab90f7bf08ea5f346b40f763cc63ba22
francisco-solis99 <franciscosoliscss@gmail.com>
2022-04-25 22:43:32 -0500

    [Add] the chip image and the credit card type image

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 
 const creditCardInputs = document.querySelectorAll('.credit-card__input');
+const imgCreditCard = document.querySelector('#creditCardImg');
 
 creditCardInputs.forEach(input => input.addEventListener('keydown', (e) => {
   e.preventDefault();
   maskText(e.key, input);
+  if (input.value.length === 1) {
+    input.value === '5' ? imgCreditCard.src = './assets/mastercard.png' : imgCreditCard.src = './assets/visa.png';
+  }
 }));
 
 function maskText(key, input) {
   const { mask, separator = '-' } = input.dataset;
+  if (key === 'Tab') return;
   if (key === 'Backspace') {
     input.value = input.value.slice(0, input.value.length - 1);
     return;
